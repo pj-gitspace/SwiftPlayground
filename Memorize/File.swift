351ad402a15157b02eb6aba61ce6673840682443
Patrick Jarvis <pjarvite@gmail.com>
2026-01-03 20:08:17 -0800

    Lecture 8 and 9 : Animation

---
+++ Memorize/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Memorize
+//
+//  Created by Patrick Jarvis on 1/2/26.
+//
+
+import Foundation
---
+++ Memorize/Memorize/FlyingNumberView.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumberView.swift
+//  Memorize
+//
+//  Created by Patrick Jarvis on 1/2/26.
+//
+
+import SwiftUI
+
+struct FlyingNumberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumberView()
+}
